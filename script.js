ed441e51d1702b2154c96ab25348f15b6ce420a6
LucasSouza159 <65685407+LucasSouza159@users.noreply.github.com>
2022-07-05 12:17:58 -0300

    a

---
+++ script.js
@@ -0,0 +1,27 @@
+function calcular(event){
+    event.preventDefault();
+
+    let volume = document.getElementById("volume").value;
+    let tempo = document.getElementById("tempo").value;
+    let opcoes = document.querySelectorAll(".calc");
+    let resultado = document.getElementById("resultado");
+    let calc;
+
+    for(i = 0; i < opcoes.length; i++){
+        if(opcoes[i].checked){
+            if(opcoes[i].value == "microgotasPorHora"){
+                calc = volume / tempo;
+                resultado.innerHTML = calc + " Microgotas por hora";
+            }else if(opcoes[i].value == "microgotasPorMinuto"){
+                calc = volume * 60 / tempo;
+                resultado.innerHTML = calc + " Microgotas por minuto";
+            }else if(opcoes[i].value == "gotasPorHora"){
+                calc = volume / tempo * 3;
+                resultado.innerHTML = calc + " Gotas por hora";
+            }else if(opcoes[i].value == "gotasPorMinuto"){
+                calc = volume * 20 / tempo;
+                resultado.innerHTML = calc + " Gotas por minuto"
+            }
+        }
+    }
+}
