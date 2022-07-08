8e961b2c9602ab6e8bea8ba8e20b2153971f8b4a
LucasSouza159 <65685407+LucasSouza159@users.noreply.github.com>
2022-07-08 09:27:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ function calcular(event){
     for(i = 0; i < opcoes.length; i++){
         if(opcoes[i].checked){
             if(opcoes[i].value == "microgotasPorHora"){
-                calc = (volume / (tempo * 60));
-                resultado.innerHTML = calc.toFixed(2) + " Microgotas por minuto";
+                calc = (volume / (tempo));
+                resultado.innerHTML = calc.toFixed(2) + " Microgotas por hora";
             }else if(opcoes[i].value == "microgotasPorMinuto"){
-                calc = (volume * 60 / (tempo * 60));
+                calc = (volume * 60 / (tempo * 60) / 60);
                 resultado.innerHTML = calc.toFixed(2) + " Microgotas por minuto";
             }else if(opcoes[i].value == "gotasPorHora"){
                 calc = (volume / (tempo * 60)) * 3;
                 resultado.innerHTML = calc.toFixed(2) + " Gotas por minuto";
             }else if(opcoes[i].value == "gotasPorMinuto"){
-                calc = volume * 20 / (tempo * 60);  
+                calc = (volume * 20 / (tempo * 60));  
                 resultado.innerHTML = calc.toFixed(2) + " Gotas por minuto"
             }
         }
